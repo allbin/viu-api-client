7db4de5663700bcf8185ab003cbfe2f64cd9a873
psc <samuel.olsson@allbinary.se>
2023-12-04 15:26:43 +0100

    Release v0.37.0

--- src/endpoints/users.ts
+++ src/endpoints/users.ts
@@ -4,12 +4,11 @@ import type { ViuApiClientOptions } from '../options';
 import type { ApiUser } from '@allbin/viu-types';
 
 interface UserOperations {
-  // FIXME: Temporarily allowing MX-818 to coexist with the old system
-  list: (ids?: string[]) => Promise<ApiUser[]>;
+  list: (ids: string[]) => Promise<ApiUser[]>;
 }
 
 export const userOperations = (opts: ViuApiClientOptions): UserOperations => ({
-  list: async (ids?: string[]) =>
+  list: async (ids: string[]) =>
     await call<undefined, ApiUser[]>('GET', `/users`, {
       ...opts,
       params: {
