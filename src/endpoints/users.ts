3a310a647efcbbc9007b9c42ce339802b7e15d08
pscz <samuel.olsson@allbinary.se>
2023-12-04 09:47:46 +0100

    MX-818: On-demand user loading (#7)
    * MX-818: Breaking change to users.list endpoint
    
    * MX-818: Temporarily allow coexistence of users findMany and findAll

--- src/endpoints/users.ts
+++ src/endpoints/users.ts
@@ -4,10 +4,16 @@ import type { ViuApiClientOptions } from '../options';
 import type { ApiUser } from '@allbin/viu-types';
 
 interface UserOperations {
-  list: () => Promise<ApiUser[]>;
+  // FIXME: Temporarily allowing MX-818 to coexist with the old system
+  list: (ids?: string[]) => Promise<ApiUser[]>;
 }
 
 export const userOperations = (opts: ViuApiClientOptions): UserOperations => ({
-  list: async () =>
-    await call<undefined, ApiUser[]>('GET', `/users`, { ...opts }),
+  list: async (ids?: string[]) =>
+    await call<undefined, ApiUser[]>('GET', `/users`, {
+      ...opts,
+      params: {
+        ...(ids ? { ids: ids.join(',') } : {}),
+      },
+    }),
 });
