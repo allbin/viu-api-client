9879e28dcdd134b5f2fcb39eeb7352bb5c33aaaa
pscz <pscz@example.com>
2024-08-21 09:55:02 +0200

    VIU-583: Removed apartments, replaced apartment sync with unit sync

--- src/endpoints/locations.ts
+++ src/endpoints/locations.ts
@@ -2,12 +2,7 @@ import call from '../call';
 
 import type { ViuApiClientOptions } from '../options';
 
-import type {
-  ApiLocation,
-  ApiLocationRequest,
-  ApiApartment,
-  ApiApartmentRequest,
-} from '@allbin/viu-types';
+import type { ApiLocation, ApiLocationRequest } from '@allbin/viu-types';
 
 interface LocationOperations {
   list: () => Promise<ApiLocation[]>;
@@ -15,22 +10,6 @@ interface LocationOperations {
   create: (location: ApiLocationRequest) => Promise<ApiLocation>;
   update: (id: string, location: ApiLocationRequest) => Promise<ApiLocation>;
   delete: (id: string) => Promise<ApiLocation>;
-
-  /** lists apartments for a location id */
-  listApartments: (location_id: string) => Promise<ApiApartment[]>;
-  getApartment: (location_id: string, id: string) => Promise<ApiApartment>;
-  createApartment: (
-    location_id: string,
-    apartment: ApiApartmentRequest,
-  ) => Promise<ApiApartment>;
-  updateApartment: (
-    location_id: string,
-    id: string,
-    apartment: ApiApartmentRequest,
-  ) => Promise<ApiApartment>;
-  /** delete all apartments for a location id */
-  deleteApartments: (location_id: string) => Promise<ApiApartment[]>;
-  deleteApartment: (location_id: string, id: string) => Promise<ApiApartment>;
 }
 
 export const locationOperations = (
@@ -54,42 +33,4 @@ export const locationOperations = (
     await call<undefined, ApiLocation>('DELETE', `/locations/${id}`, {
       ...opts,
     }),
-  listApartments: async (location_id) =>
-    await call<undefined, ApiApartment[]>(
-      'GET',
-      `/locations/${location_id}/apartments`,
-      {
-        ...opts,
-      },
-    ),
-  getApartment: async (location_id, id) =>
-    await call<undefined, ApiApartment>(
-      'GET',
-      `/locations/${location_id}/apartments/${id}`,
-      { ...opts },
-    ),
-  createApartment: async (location_id, apartment) =>
-    await call<ApiApartmentRequest, ApiApartment>(
-      'POST',
-      `/locations/${location_id}/apartments`,
-      { ...opts, body: apartment },
-    ),
-  updateApartment: async (location_id, id, apartment) =>
-    await call<ApiApartmentRequest, ApiApartment>(
-      'PUT',
-      `/locations/${location_id}/apartments/${id}`,
-      { ...opts, body: apartment },
-    ),
-  deleteApartments: async (location_id) =>
-    await call<undefined, ApiApartment[]>(
-      'DELETE',
-      `/locations/${location_id}`,
-      { ...opts },
-    ),
-  deleteApartment: async (location_id, id) =>
-    await call<undefined, ApiApartment>(
-      'DELETE',
-      `/locations/${location_id}/apartments/${id}`,
-      { ...opts },
-    ),
 });
--- src/endpoints/sync.ts
+++ src/endpoints/sync.ts
@@ -1,15 +1,15 @@
-import { ApiSyncApartmentsRequest } from '@allbin/viu-types';
+import { ApiSyncUnitsRequest } from '@allbin/viu-types';
 import call from '../call';
 
 import type { ViuApiClientOptions } from '../options';
 
 interface SyncOperations {
-  apartments: (data: ApiSyncApartmentsRequest) => Promise<void>;
+  apartments: (data: ApiSyncUnitsRequest) => Promise<void>;
 }
 
 export const syncOperations = (opts: ViuApiClientOptions): SyncOperations => ({
   apartments: async (data) => {
-    await call<ApiSyncApartmentsRequest, void>('POST', `/sync/apartments`, {
+    await call<ApiSyncUnitsRequest, void>('POST', `/sync/units`, {
       ...opts,
       body: data,
     });
