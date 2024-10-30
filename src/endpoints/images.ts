d0b38d55efe1a33a5c53814603845353a45d347a
pscz <pscz@example.com>
2024-10-30 12:43:55 +0100

    images upload route should accept basic File objects

--- src/endpoints/images.ts
+++ src/endpoints/images.ts
@@ -1,12 +1,11 @@
 import call from '../call';
 
 import type { ViuApiClientOptions } from '../options';
-import type { ApiClientUploadFile } from './attachments';
 
 import type { ApiImageUploadResponse } from '@allbin/viu-types';
 
 interface ImageOperations {
-  create: (files: ApiClientUploadFile[]) => Promise<ApiImageUploadResponse[]>;
+  create: (files: File[]) => Promise<ApiImageUploadResponse[]>;
 }
 
 export const imageOperations = (
@@ -15,8 +14,8 @@ export const imageOperations = (
   create: async (files) => {
     const data = new FormData();
 
-    for (const uf of files) {
-      data.append('files', uf.file);
+    for (const f of files) {
+      data.append('files', f);
     }
 
     return await call<undefined, ApiImageUploadResponse[]>('POST', `/images`, {
